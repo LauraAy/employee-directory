174516d82af2f745a8b10475325c1decdf464550
LauraAy <laura.ay1564@gmail.com>
2020-03-22 20:23:10 -0700

    successful employee list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import data from './MOCK_DATA'
+import data from './MOCK_DATA.json'
 
 
-class App extends Component {
-
-
+class App extends Component { 
 
   render(){
-    return(
+    return (
       <>
-      <div>Hello World</div>
-      <ul>
-       
-      </ul>
-      </>
+        <div className="jumbotron">
+          <h1 className="display-4">Employee Directory</h1>
+        </div>
+        {data.map((employee, index) => {
+        return <div>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{employee.first_name} {employee.last_name}</h5>  
+              <h6>{employee.email}</h6> 
+              <h6>{employee.gender}</h6>
+            </div>
+          </div>  
+        </div>  
+      })}
+   </>
     )
   }
 }
